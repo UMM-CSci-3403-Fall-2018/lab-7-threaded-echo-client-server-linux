f9f65e725b37494e6991776bfb94e807c667bc6a
plumstyle <anxxx154@morris.umn.edu>
2018-11-01 11:19:33 -0500

    Add Server class in EchoServer. It creates a new thread for each client, and all tests now pass.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -17,13 +17,37 @@ public class EchoServer {
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
+			Thread t = new Thread(new Server(serverSocket));
+			t.start();
+		}
+	}
+	class Server implements Runnable {
+		private Socket socket;
+		public Server(ServerSocket serverSocket) {
+			try {
+				this.socket = serverSocket.accept();
+			} catch (IOException e){System.out.println(e);}
+		}
+
+		public void run() {
+			try {
+				InputStream inputStream = socket.getInputStream();
+				OutputStream outputStream = socket.getOutputStream();
+				int b;
+				while ((b = inputStream.read()) != -1) {
+					outputStream.write(b);
+				}
+				socket.shutdownOutput();
 			}
+			catch (IOException e){System.out.println(e);}
+			finally {
+				// always close the socket
+				try {
+					socket.close();
+				} catch (IOException e){System.out.println(e);}
+
+			}
+
 		}
 	}
 }
